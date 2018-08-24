616b02a0a7506de67037123e5c151be2f37fd8e0
Borui <wbrzju@outlook.com>
2018-08-24 17:37:01 -0400

    ReDesign: refined thread create page error feedback

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -13,33 +13,31 @@
 
                         <div class="form-group">
                             <label for="channel_id">Channel:</label>
-                            <select name="channel_id" id="channel_id" class="form-control" required>
+                            <select name="channel_id" id="channel_id" required
+                                    class="form-control {{ $errors->has('channel') ? 'is-invalid' : (count($errors) ? 'is-valid' : '') }}">
                                 <option value="">Choose one...</option>
                                 @foreach ($channels as $channel)
-                                    <option value="{{$channel->id}}" {{old('channel_id') == $channel->id ? 'select' : ''}}>
+                                    <option value="{{$channel->id}}" {{old('channel_id') == $channel->id ? 'selected' : ''}}>
                                         {{$channel->name}}
                                     </option>
                                 @endforeach
                             </select>
+                            @include('threads.form-feedback', ['object' => 'channel'])
                         </div>
                         <div class="form-group">
                             <label for="title">Title:</label>
-                            <input type="text" class="form-control" name="title" id="title" value="{{old('title')}}" required>
+                            <input type="text" name="title" id="title" value="{{old('title')}}" required
+                                    class="form-control {{ $errors->has('title') ? 'is-invalid' : (count($errors) ? 'is-valid' : '') }}">
+                            @include('threads.form-feedback', ['object' => 'title'])
                         </div>
                         <div class="form-group">
                             <label for="body">Body:</label>
-                            <textarea class="form-control" name="body" id="body" rows="10" required>{{old('body')}}</textarea>
+                            <textarea class="form-control {{ $errors->has('body') ? 'is-invalid' : (count($errors) ? 'is-valid' : '') }}"
+                                    name="body" id="body" rows="10" required> {{ old('body') }}
+                            </textarea>
+                            @include('threads.form-feedback', ['object' => 'body'])
                         </div>
                         <button class="form-group btn btn-default">Post</button>
-                        @if (count($errors))
-                        <div class="form-group">
-                            <ul class="form-control alert alert-danger">
-                                @foreach ($errors->all() as $error)
-                                    <li>{{$error}}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                        @endif
                     </form>
                 </div>
             </div>
